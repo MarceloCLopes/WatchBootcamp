4779b069129b42c88bcfcde2505ce009bbc2d39c
MarceloCLopes <marc01mcl@gmail.com>
2021-04-12 17:11:44 -0300

    initial commit modulo wear

---
+++ wear/src/main/java/com/mcl/wear/MealActivity.kt
@@ -0,0 +1,12 @@
+package com.mcl.wear
+
+import android.app.Activity
+import android.os.Bundle
+
+class MealActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
