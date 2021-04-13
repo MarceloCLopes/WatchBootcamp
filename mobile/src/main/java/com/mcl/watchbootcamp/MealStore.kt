7429c52911e924b16d2852cd6c1278c5b8064481
MarceloCLopes <marc01mcl@gmail.com>
2021-04-13 15:30:21 -0300

    updated meal store, implemented meal shared

--- mobile/src/main/java/com/mcl/watchbootcamp/MealStore.kt
+++ mobile/src/main/java/com/mcl/watchbootcamp/MealStore.kt
@@ -3,6 +3,7 @@ package com.mcl.watchbootcamp
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.mcl.shared.Meal
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
