e76e719b7f8aded64f9ed45c2ed3d605f3bf7612
MarceloCLopes <marc01mcl@gmail.com>
2021-04-13 15:28:55 -0300

    updated meal list activity from functions

--- mobile/src/main/java/com/mcl/watchbootcamp/MealListActivity.kt
+++ mobile/src/main/java/com/mcl/watchbootcamp/MealListActivity.kt
@@ -1,14 +1,22 @@
 package com.mcl.watchbootcamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
+import com.mcl.shared.Meal
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MealListActivity : AppCompatActivity(), MealListAdapter.Callback {
+class MealListActivity : AppCompatActivity(),
+    MealListAdapter.Callback,
+    GoogleApiClient.ConnectionCallbacks {
 
     private var adapter: MealListAdapter? = null
+    private lateinit var client: GoogleApiClient
+    private var connectedNode: List<Node>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +26,35 @@ class MealListActivity : AppCompatActivity(), MealListAdapter.Callback {
         adapter = MealListAdapter(meals, this)
         list.adapter = adapter as MealListAdapter
         list.layoutManager = LinearLayoutManager(this)
+
+        client = GoogleApiClient.Builder(this)
+            .addApi(Wearable.API)
+            .addConnectionCallbacks(this)
+            .build()
+        client.connect()
+
     }
 
     override fun mealClicked(meal: Meal) {
-        TODO("Not yet implemented")
+        val gson = Gson()
+        connectedNode?.forEach{node ->
+            val bytes = gson.toJson(meal).toByteArray()
+            Wearable.MessageApi.sendMessage(client, node.id, "/meal", bytes)
+        }
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+        Wearable.NodeApi.getConnectedNodes(client).setResultCallback {
+            connectedNode = it.nodes
+
+            Wearable.DataApi.addListener(client) { data ->
+                val meal = Gson().fromJson(String(data[0].dataItem.data), Meal::class.java)
+                adapter?.updateMeals(meal)
+            }
+        }
+    }
+
+    override fun onConnectionSuspended(code: Int) {
+       connectedNode = null
     }
 }
