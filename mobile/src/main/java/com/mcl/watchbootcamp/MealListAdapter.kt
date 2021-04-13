78d8049900a7a4b648944ed84de93edd3ff741c4
MarceloCLopes <marc01mcl@gmail.com>
2021-04-13 15:29:46 -0300

    updated meal list adapter

--- mobile/src/main/java/com/mcl/watchbootcamp/MealListAdapter.kt
+++ mobile/src/main/java/com/mcl/watchbootcamp/MealListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.mcl.shared.Meal
 import kotlinx.android.synthetic.main.adapter_mael.view.*
 
 class MealListAdapter(
